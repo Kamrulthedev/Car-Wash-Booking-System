d7ebe76507b90c79a9886f580fddd0c3b3f4d959
Kamrul Hassan <kamrulthedev@gmail.com>
2024-08-30 09:42:22 +0600

    update-page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/layouts/MainLayout";
 import Home from "../pages/home/home/Home";
 import Login from "../pages/login/Login";
-import Register from "../pages/home/register/Register";
+import Register from "../pages/register/Register";
 
 const router = createBrowserRouter([
   {
